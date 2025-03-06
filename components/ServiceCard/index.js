cb851f6277d70ad7970f88a679a19d00b1e073f1
sre <sreshtaar3@gmail.com>
2025-03-06 17:08:23 +0530

    updated

--- components/ServiceCard/index.js
+++ components/ServiceCard/index.js
@@ -18,7 +18,7 @@ const ServiceCard = ({ name, description }) => {
       <p className="mt-5 opacity-40 text-xl">
         {description
           ? description
-          : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "}
+          : " "}
       </p>
     </div>
   );
